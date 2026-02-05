8b614980b79e229a10392343d77061bcd938cdc6
fmw343 <fmw343@gmail.com>
2026-02-05 19:34:34 +0530

    improved by removing divisors list

--- exercise11.py
+++ exercise11.py
@@ -2,7 +2,7 @@ def get_integer():
     return int(input("Give me a number: "))
 
 def check_prime(number):
-    divisors = []
+    is_prime = True
     
     if number < 2:
         print(f"{number} is not a prime.")
@@ -10,12 +10,11 @@ def check_prime(number):
     
     for x in range(2, number):
         if number % x == 0:
-            divisors.append(number)
-        if len(divisors) > 0:
+            is_prime = False
             print(f"{number} is not a prime.")
             break
         
-    if len(divisors) == 0:
+    if is_prime == True:
         print(f"{number} is a prime.")
  
 num = get_integer()
