025d6c8ae88b09cd6d6d4fa179aecac48b188b8e
fmw343 <fmw343@gmail.com>
2026-02-05 19:27:48 +0530

    fixed solution to account for 0 and negative integers

--- exercise11.py
+++ exercise11.py
@@ -3,14 +3,20 @@ def get_integer():
 
 def check_prime(number):
     divisors = []
+    
+    if number < 1:
+        print(f"{number} is not a prime.")
+        return
+    
     for x in range(2, number - 1):
         if number % x == 0:
-            divisors.append(x)
-    
-    if len(divisors) > 0:
-        print(f"{number} is not a prime.")
-    else:
-        print(f"{number} is a prime.")
+            divisors.append(number)
+        if len(divisors) > 0:
+            print(f"{number} is not a prime.")
+            break
         
+    if len(divisors) == 0:
+        print(f"{number} is a prime.")
+ 
 num = get_integer()
 check_prime(num)
